1ad1600d331b3827cc02e0455c7742d167566106
ParkJongHa <zoyngm@gmail.com>
2023-02-27 18:30:56 +0900

    SLinkedList 테스트

--- src/dft/jcf/list/demo001_singlylinkedlist/SLinkedList.java
+++ src/dft/jcf/list/demo001_singlylinkedlist/SLinkedList.java
@@ -1,4 +1,197 @@
 package dft.jcf.list.demo001_singlylinkedlist;
 
-public class SLinkedList {
+import java.util.*;
+import java.util.function.UnaryOperator;
+
+public class SLinkedList<E> implements List<E> {
+
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
+
+    public SLinkedList() {
+        this.head = null;
+        this.tail = null;
+        this.size = 0;
+    }
+
+    private Node<E> search(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        Node<E> x = head;
+
+        for (int i=0; i<index; i++) {
+            x = x.next;
+        }
+
+        return x;
+    }
+
+
+    public void addFirst(E value) {
+        Node<E> newNode = new Node<E>(value);
+        newNode.next = head;
+        head = newNode;
+        size++;
+
+        if (head.next == null) {
+            tail = head;
+        }
+    }
+
+    @Override
+    public boolean add(E value) {
+        addLast(value);
+        return false;
+    }
+
+    public void addLast(E value) {
+        Node<E> newNode = new Node<E>(value);
+
+        if (size == 0) {
+            addFirst(value);
+            return;
+        }
+
+        tail.next = newNode;
+        tail = newNode;
+        size++;
+    }
+
+    @Override
+    public void add(int index, E element) {
+
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return false;
+    }
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void replaceAll(UnaryOperator<E> operator) {
+        List.super.replaceAll(operator);
+    }
+
+    @Override
+    public void sort(Comparator<? super E> c) {
+        List.super.sort(c);
+    }
+
+    @Override
+    public Spliterator<E> spliterator() {
+        return List.super.spliterator();
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return null;
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        return null;
+    }
 }
